d2776422540b86a1de2b4afddec7157aaedae681
arvin-tan <arvin_tan@pervasivedisplays.com>
2024-05-03 10:51:26 +0800

    Update EPD_Driver.cpp

--- src/EPD_Driver.cpp
+++ src/EPD_Driver.cpp
@@ -283,8 +283,7 @@ void EPD_Driver::COG_initial_GU()
 		while ( digitalRead( spi_basic.panelBusy ) != LOW );
 
 		_sendIndexData( 0x1a, &register_data[0], 1 );  //Input Temperature: 25C
-		_sendIndexData( 0x3c, &register_data[2], 1 );  //Border settings 0xc0
-		_sendIndexData( 0x22, &register_data[3], 1 );  //PSR 0xdf
+		_sendIndexData( 0x22, &register_data[1], 1 );  //PSR 0xdf
 	}
 	else
 	{
